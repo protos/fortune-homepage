2571326afa424a3937dc92b0e379cbcedd58eecb
Colm De Barra <cdebarra@marktplaats.nl>
2017-09-22 17:19:58 +0200

    Fortune cookie git add -A

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 var express = require('express');
+var fs = require('fs');
+
 var app = express();
+var fortunesArray = [];
+
 app.get('/', function (req, res) {
-    res.send('Hello Colm');
+
+    function getRandomInteger() {
+        var min = Math.ceil(0);
+        var max = Math.floor(fortunesArray.length);
+        return Math.floor(Math.random() * (max - min) + min);
+    };
+
+    res.send(fortunesArray[getRandomInteger()]);
 });
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
+
+    fs.readFile('fortune.dat', 'utf8', function(err, data) {
+        console.log ('data: ' + data);
+        fortunesArray = data.split('###');
+    })
 });
