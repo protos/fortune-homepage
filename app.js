15257343b925b4ae4b7a313d430251955ac9ac86
Colm De Barra <kildimo@gmail.com>
2017-09-23 11:48:25 +0200

    ES6 Beginnings

--- app.js
+++ app.js
@@ -4,21 +4,27 @@ var fs = require('fs');
 var app = express();
 var fortunesArray = [];
 
+
+var readFile = (filePath) => {
+    fs.readFile(filePath, 'utf8', function(err, data) {
+        //console.log ('data: ' + data);
+        fortunesArray = data.split('###');
+    })    
+}
+
+var getRandomInteger = (min, max) => {
+//    var min = Math.ceil(0);
+//    var max = Math.floor(fortunesArray.length);
+    return Math.floor(Math.random() * (max - min) + min);
+};
+
+
 app.get('/', function (req, res) {
-
-    function getRandomInteger() {
-        var min = Math.ceil(0);
-        var max = Math.floor(fortunesArray.length);
-        return Math.floor(Math.random() * (max - min) + min);
-    };
-
-    res.send(fortunesArray[getRandomInteger()]);
+    res.send(fortunesArray[getRandomInteger(0, fortunesArray.length)]);
 });
+
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
-
-    fs.readFile('fortune.dat', 'utf8', function(err, data) {
-        console.log ('data: ' + data);
-        fortunesArray = data.split('###');
-    })
+    readFile('fortune.dat');
 });
