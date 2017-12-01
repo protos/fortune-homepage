d80dd6ce2c735b4d992a24096007bf8884fb56fe
Colm De Barra <cdebarra@marktplaats.nl>
2017-12-01 16:26:57 +0100

    Added endpoint to proxy spacestation location.

--- api/config.js
+++ api/config.js
@@ -7,5 +7,6 @@ module.exports = {
     port: process.env.PORT || 3001,
     db: {
         uri: 'mongodb://localhost:27017/fortunes',
-    }
+    },
+    spaceStationUrl: 'http://api.open-notify.org/iss-now.json'
 };
--- api/routes.js
+++ api/routes.js
@@ -1,6 +1,12 @@
 'use strict';
 
 module.exports = function(ctx) {
+    /**
+     * Module Dependencies
+     */
+    const http = require('http');
+    const config  = require('./config');
+
     const db     = ctx.db,
           server = ctx.server;
 
@@ -26,6 +32,21 @@ module.exports = function(ctx) {
         .catch((err) => {
             res.send(200, "Something horrible has happened !!!");
             next()
+        });
+    });
+
+    server.get('/space-station-location', (req, res, next) => {
+        http.get(config.spaceStationUrl, (resp) => {
+            let data = '';
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            resp.on('end', () => {
+                res.send(JSON.parse(data));
+            })
+        }).on('error', (err) => {
+            res.send(200, "Space station error. It has crashed.");
         });
     });
 
